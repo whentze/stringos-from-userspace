7a50c661377a4c98f732e2e5f36573445cb67863
muvlon <muvlon@hentzes.de>
2023-06-02 00:46:28 +0200

    sus prompt

--- src/bin/sus.rs
+++ src/bin/sus.rs
@@ -1,10 +1,20 @@
+use std::io::Write;
+
 use anyhow::Error;
 
 use stringos_from_userspace::the_syscall;
 
+fn prompt() -> Result<(), Error> {
+    print!("> ");
+    std::io::stdout().flush()?;
+    Ok(())
+}
+
 fn main() -> Result<(), Error> {
+    prompt()?;
     for line in std::io::stdin().lines() {
         println!("{}", the_syscall(line?));
+        prompt()?;
     }
     Ok(())
 }
